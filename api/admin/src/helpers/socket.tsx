6bf5d1a71bfbb581f3db2b69406d4c217ce7b8af
elvin <elvin.bayramov@protonmail.com>
2019-12-07 22:10:17 +0400

    Production bug fix

--- api/admin/src/helpers/socket.tsx
+++ api/admin/src/helpers/socket.tsx
@@ -1,5 +1,5 @@
 import openSocket from 'socket.io-client';
-const socket = openSocket('http://localhost:3333');
+const socket = openSocket(process.env.NODE_ENV === 'production' ? '' : 'http://localhost:3333'); 
 
 export function subscribe(event: string, callback: (data: any) => void) {
   socket.on(event, (data: any) => callback(data))
